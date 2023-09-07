f68a86e926cd9ff8cd0cf1ccbe9f6e28373f7087
Dusty Dump <dumpdusty@gmail.com>
2023-09-06 20:27:57 -0700

    add env variables

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -4,33 +4,25 @@ import 'dotenv/config'
 
 describe('Authentication', () => {
   describe('Authentication with valid credentials', () => {
-    it('validate status code', async () => {
-      let res
+    let res
+    before(async () => {
       res = await request(process.env.BASE_URL)
         .post('/user/login')
         .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
-
-      // console.log(res.body)
-
-      expect(res.statusCode).to.eq(200)
     })
 
-    it('validate response message', async () => {
-      let res
-      res = await request(process.env.BASE_URL)
-        .post('/user/login')
-        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
+    it('validate status code', async () => {
+      await expect(res.statusCode).to.eq(200)
+    })
 
+    it('validate response message', () => {
       expect(res.body.message).to.eq('Auth success')
     })
 
-    it('check the token exist', async () => {
-      let res
-      res = await request(process.env.BASE_URL)
-        .post('/user/login')
-        .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
-
+    it('check the token exist', () => {
       expect(res.body.payload.token).to.be.a('string')
     })
   })
+
+  describe('Auth with invalid credentials', () => {})
 })
