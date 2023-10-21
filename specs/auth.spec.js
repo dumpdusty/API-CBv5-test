47db2b1e3159ef9f29a5fb4ae54752b2ad44d77e
Dusty Dump <dumpdusty@gmail.com>
2023-10-20 18:02:09 -0700

    add clients tests add client helper

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -10,10 +10,12 @@ describe('Authentication', () => {
     })
 
     it('validate status code', async () => {
+      console.log('first')
       await expect(res.statusCode).to.eq(200)
     })
 
     it('validate response message', () => {
+      console.log('second')
       expect(res.body.message).to.eq('Auth success')
     })
 
@@ -21,21 +23,17 @@ describe('Authentication', () => {
       expect(res.body.payload.token).to.be.a('string')
     })
   })
-
   describe('Auth with invalid credentials', () => {
     let res
     before(async () => {
       res = await login('invalid', 'invalid')
     })
-
     it('validate status code', async () => {
       await expect(res.statusCode).to.eq(400)
     })
-
     it('validate response message', () => {
       expect(res.body.message).to.eq('Auth failed')
     })
-
     it('check the token exist', () => {
       expect(res.body.payload).to.not.haveOwnProperty('token')
     })
